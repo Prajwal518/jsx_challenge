3618384b5ea0e1f545459ed5b751b8d71e1f49e5
Prajwal Gowda S <65961288+Prajwal518@users.noreply.github.com>
2021-02-13 12:17:59 +0530

    starting 1.Create a react app from scratch 2.It should display a h1 heading 3.It should display an unordered list (bullet points) 4.It should contain 3 list elements.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 //Create a react app from scratch.
+var React = require("react");
+var ReactDom = require("react-dom");
 //It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+ReactDom.render(
+  <div>
+    <h1>my fav Anime</h1>
+    {/* It should display an unordered list (bullet points). */}
+    <ul>
+      {/* It should contain 3 list elements. */}
+      <li>One Piece</li>
+      <li>Death Note</li>
+      <li>Ace of Diamond</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
